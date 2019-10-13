6b0143ad19e2582b4e5450fb37a1e4d593bb5bca
Agustin Bally <agustinbally@gmail.com>
2019-10-13 17:55:52 -0300

    modifico scripts para ejecutar tests

--- program-tests.bat
+++ program-tests.bat
@@ -1,5 +1,9 @@
+@echo off
+
 dotnet build -c Release
 
+echo -- Ejecucion caso mutante --
 dotnet MutantDetector.Program/bin/Release/netcoreapp2.2/MutantDetector.Program.dll ATGCGA CAGTGC TTATGT AGAAGG CCCCTA TCACTG
 
+echo -- Ejecucion caso no mutante --
 dotnet MutantDetector.Program/bin/Release/netcoreapp2.2/MutantDetector.Program.dll ATGCGA CCGTGC TTATGT AGAAGG TACGTA TCACTG
